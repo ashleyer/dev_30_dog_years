4a0e155e11b93caaf32fd84d3513fbfde82e0a79
ashleyromano <ashleye.romano@gmail.com>
2022-04-28 15:37:55 -0400

    project complete

---
+++ main.js
@@ -0,0 +1,18 @@
+
+let myAge = 37;
+
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+// console.log(laterYears);
+// console.log(earlyYears);
+
+laterYears *= 4;
+let myAgeInDogYears = earlyYears + laterYears;
+//console.log(myAgeInDogYears);
+
+let myName = 'Ashley'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years, or ${myAgeInDogYears} years old in dog years.`);
